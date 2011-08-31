cdc6b8e3198ad6acf6acba7a47b9f611423781c8
John Reese <john@noswap.com>
2011-08-31 18:46:00 -0400

    Example GUI window
    Issue #1332

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <QtGui>
 #include <okuda.h>
 
 int main(int argc, char **argv)
 {
+	QApplication app(argc, argv);
+
+	QTextEdit *textEdit = new QTextEdit;
+	QPushButton *quitButton = new QPushButton("Quit");
+
+	QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(textEdit);
+	layout->addWidget(quitButton);
+
+	QWidget *window = new QWidget;
+	window->setLayout(layout);
+	window->show();
+
 	Okuda *o = new Okuda;
 	printf("o->number: %d\n", o->number);
 	printf("o->getmynumber(): %d\n", o->getmynumber());
 
-	return 0;
+	return app.exec();
 }
